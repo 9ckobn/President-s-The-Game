534d4b2b3ed54b55550c21d16b2f7ce4d9370a4c
DESKTOP-7MM2106\unity <aleksander.conushin>
2022-10-24 15:52:36 +0700

    Edit scrolling

--- Assets/1_Scripts/UI/Components/ScrollCards.cs
+++ Assets/1_Scripts/UI/Components/ScrollCards.cs
@@ -6,10 +6,10 @@ namespace UI.Components
 {
     public class ScrollCards : MonoBehaviour
     {
-        private const int CARD_ID_LINE = 3;
+        private const int CARDS_IN_LINE = 3, HEIGHT_LINE = 300;
 
         [SerializeField] private LineCard prefabLineCard;
-        [SerializeField] private GameObject linesParent;
+        [SerializeField] private GameObject contentLinesParent;
 
         private List<LineCard> linesCard = new List<LineCard>();
 
@@ -22,8 +22,11 @@ namespace UI.Components
             {
                 if (countCards == 0)
                 {
-                    line = Instantiate(prefabLineCard, linesParent.transform);
+                    line = Instantiate(prefabLineCard, contentLinesParent.transform);
                     linesCard.Add(line);
+
+                    float prevHeight = contentLinesParent.GetComponent<RectTransform>().rect.height;
+                    contentLinesParent.GetComponent<RectTransform>().sizeDelta = new Vector2(0, prevHeight + HEIGHT_LINE);
                 }
 
                 line.AddCard(cardsData[c].gameObject);
