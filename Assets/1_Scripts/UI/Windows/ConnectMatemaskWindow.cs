7b1004ec1ba91aba99436de50938f9b82cbbd3b0
DESKTOP-7MM2106\unity <aleksander.conushin>
2022-10-26 09:31:42 +0700

    Edit ui init scene

--- Assets/1_Scripts/UI/Windows/ConnectMatemaskWindow.cs
+++ Assets/1_Scripts/UI/Windows/ConnectMatemaskWindow.cs
@@ -6,13 +6,12 @@ namespace UI
 {
     public class ConnectMatemaskWindow : Window
     {
-        [SerializeField] private Button connectButton, mainPageButton;
+        [SerializeField] private Button connectButton;
         [SerializeField] private GameObject qrImage;
 
         protected override void AfterInitialization()
         {
             connectButton.onClick.AddListener(ClickConnectButton);
-            mainPageButton.onClick.AddListener(ClickMainPageButton);
         }
 
         public void SetMethodConnect(bool qrMethod)
@@ -27,11 +26,6 @@ namespace UI
                 connectButton.gameObject.SetActive(true);
                 qrImage.gameObject.SetActive(false);
             }
-        }
-
-        private void ClickMainPageButton()
-        {
-            AppManager.Instance.GoToMainPageMetaverse();
         }
 
         private void ClickConnectButton()
