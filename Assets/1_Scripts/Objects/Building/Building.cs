8a00a67e7cb15cedc7be38e7942137f394dc3dbd
DESKTOP-7MM2106\unity <aleksander.conushin>
2022-10-30 10:01:35 +0700

    Create logo rotate animation

--- Assets/1_Scripts/Objects/Building/Building.cs
+++ Assets/1_Scripts/Objects/Building/Building.cs
@@ -1,35 +1,43 @@
 using EffectSystem;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Buildings
 {
     [RequireComponent(typeof(BoxCollider))]
     public class Building : MonoBehaviour
     {
-        [SerializeField]private GameObject model;
+        [SerializeField] private GameObject model;
         [SerializeField] private GameObject logo;
         [SerializeField] private GameObject effectExplosion;
         [SerializeField] private TypeAttribute typeBuilding;
 
-        public TypeAttribute GetTypeBuilding{ get => typeBuilding; }
+        public TypeAttribute GetTypeBuilding { get => typeBuilding; }
 
         private bool isTarget;
 
+        private void Start()
+        {
+        }
+
         private void OnMouseUp()
         {
-            
+
         }
 
         private void OnMouseDown()
         {
-            
+
         }
 
         public void EnableStateTarget()
         {
             isTarget = true;
 
-            Debug.Log("state target");
+            Vector3 rotate = new Vector3(logo.transform.rotation.x, 360, logo.transform.rotation.z);
+
+            logo.transform.DORotate(rotate, 2f, RotateMode.FastBeyond360)
+                .SetLoops(-1, LoopType.Restart);
         }
     }
 }
